a7449b54a781a3135386db5e7ac9b76d451b4341
Nourmohamed445 <iamstudingpython312@gmail.com>
2025-08-07 13:50:55 +0300

    Update ddl_silver.sql

--- Scripts /silver/ddl_silver.sql
+++ Scripts /silver/ddl_silver.sql
@@ -5,12 +5,12 @@ IF OBJECT_ID('Silver.crm_cust_info', 'U') IS NOT NULL
 GO
 CREATE TABLE Silver.crm_cust_info 
 (
-	cst_id					INT ,
-	cst_key					NVARCHAR(50),
+	cst_id				INT ,
+	cst_key				NVARCHAR(50),
 	cst_firstname			NVARCHAR(50),
 	cst_lastname			NVARCHAR(50),
 	cst_marital_status		NVARCHAR(50),
-	cst_gndr				NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
 	cst_create_date			DATE,
 	dwh_create_date			DATETIME2 DEFAULT GETDATE()
 );
